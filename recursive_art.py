aa69a032bcd3a56105f0562ad2fc0b44aa9cb20a
jkunimune15 <jkunimune15@punahou.edu>
2016-04-10 14:56:35 -0400

    Implemented numpy versions of parametric and HSV

--- recursive_art.py
+++ recursive_art.py
@@ -10,7 +10,7 @@ from pylab import*
 from scipy.io import wavfile
 
 
-def build_random_function(min_depth, max_depth, varis):
+def build_random_function(min_depth, max_depth, varis=[["x"],["y"]]):
     """ Builds a random function of depth at least min_depth and depth
         at most max_depth (see assignment writeup for definition of depth
         in this context)
@@ -29,11 +29,15 @@ def build_random_function(min_depth, max_depth, varis):
             return ["prod", build_random_function(min_depth-1,max_depth-1,varis), build_random_function(min_depth-1,max_depth-1,varis)]
         if r < 2.0:
             return ["avg", build_random_function(min_depth-1,max_depth-1,varis), build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 3.0:
+        if r < 2.7:
             return ["sin_pi", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 4.0:
+        if r < 3.4:
             return ["cos_pi", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 4.5:
+        if r < 3.9:
+            return ["cos_4", build_random_function(min_depth-1,max_depth-1,varis)]
+        if r < 4.4:
+            return ["sin_4", build_random_function(min_depth-1,max_depth-1,varis)]
+        if r < 4.7:
             return ["cos_30", build_random_function(min_depth-1,max_depth-1,varis)]
         if r < 5.0:
             return ["sin_30", build_random_function(min_depth-1,max_depth-1,varis)]
@@ -42,28 +46,28 @@ def build_random_function(min_depth, max_depth, varis):
         if r < 6.0:
             return ["neg", build_random_function(min_depth-1,max_depth-1,varis)]
         if r < 6.5:
-            return ["square", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 7.0:
             return ["cube", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 7.5:
+        if r < 7.0:
             return ["square_root", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 8.0:
+        if r < 7.5:
             return ["lnabs", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 8.5:
+        if r < 8.0:
             return ["abs", build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 9.0:
+        if r < 8.5:
             return ["hypot", build_random_function(min_depth-1,max_depth-1,varis), build_random_function(min_depth-1,max_depth-1,varis)]
-        if r < 9.5:
+        if r < 9.4:
+            return ["square", build_random_function(min_depth-1,max_depth-1,varis)]
+        if r < 9.7:
             return ["copysign", build_random_function(min_depth-1,max_depth-1,varis), build_random_function(min_depth-1,max_depth-1,varis)]
         if r < 10.0:
             return ["pow", build_random_function(min_depth-1,max_depth-1,varis), build_random_function(min_depth-1,max_depth-1,varis)]
 
 
-def evaluate(f, x, y):
+def evaluate(f, x, y, z=None):
     """
     evaluates a function over a set of points
     f = a list representing the function
-    x, y = a numpy array of floats representing a coordinate system
+    x, y, z, t = a numpy array of floats representing a coordinate system
     returns a numpy array of floats in range [-1.0,1.0]
 
     >>> evaluate(["avg", ["x"],["y"]], np.array([1.0, 0.0]), np.array([0.5, 0.5]))
@@ -73,39 +77,45 @@ def evaluate(f, x, y):
         ans = x
     elif f[0] == "y":
         ans = y
+    elif f[0] == "z":
+        ans = z
     elif f[0] == "prod":
-        ans = evaluate(f[1],x,y) * evaluate(f[2],x,y)
+        ans = evaluate(f[1],x,y,z=z) * evaluate(f[2],x,y,z=z)
     elif f[0] == "avg":
-        ans = 0.5*(evaluate(f[1],x,y) + evaluate(f[2],x,y))
+        ans = 0.5*(evaluate(f[1],x,y,z=z) + evaluate(f[2],x,y,z=z))
     elif f[0] == "cos_pi":
-        ans = np.cos(pi * evaluate(f[1],x,y))
+        ans = np.cos(pi * evaluate(f[1],x,y,z=z))
     elif f[0] == "sin_pi":
-        ans = np.sin(pi * evaluate(f[1],x,y))
+        ans = np.sin(pi * evaluate(f[1],x,y,z=z))
+    elif f[0] == "cos_4":
+        ans = np.cos(4 * evaluate(f[1],x,y,z=z))
+    elif f[0] == "sin_4":
+        ans = np.sin(4 * evaluate(f[1],x,y,z=z))
     elif f[0] == "cos_30":
-        ans = np.cos(30 * evaluate(f[1],x,y))
+        ans = np.cos(30 * evaluate(f[1],x,y,z=z))
     elif f[0] == "sin_30":
-        ans = np.sin(30 * evaluate(f[1],x,y))
+        ans = np.sin(30 * evaluate(f[1],x,y,z=z))
     elif f[0] == "tan_pi/4":
-        ans = np.tan(pi/4.0 * evaluate(f[1],x,y))
+        ans = np.tan(pi/4.0 * evaluate(f[1],x,y,z=z))
     elif f[0] == "neg":
-        ans = np.negative(evaluate(f[1],x,y))
+        ans = np.negative(evaluate(f[1],x,y,z=z))
     elif f[0] == "square":
-        ans = np.square(evaluate(f[1],x,y))
+        ans = np.square(evaluate(f[1],x,y,z=z))
     elif f[0] == "cube":
-        ans = np.power(evaluate(f[1],x,y), 3)
+        ans = np.power(evaluate(f[1],x,y,z=z), 3)
     elif f[0] == "square_root":
-        ans = evaluate(f[1],x,y)
+        ans = evaluate(f[1],x,y,z=z)
         ans = np.copysign(np.sqrt(np.absolute(ans)), ans)
     elif f[0] == "lnabs":
-        ans = np.log(np.absolute(evaluate(f[1],x,y))+1) / log(2)
+        ans = np.log(np.absolute(evaluate(f[1],x,y,z=z))+1) / log(2)
     elif f[0] == "abs":
-        ans = np.absolute(evaluate(f[1],x,y)) *2-1
+        ans = np.absolute(evaluate(f[1],x,y,z=z)) *2-1
     elif f[0] == "hypot":
-        ans = np.hypot(evaluate(f[1],x,y), evaluate(f[2],x,y)) *sqrt(2)-1
+        ans = np.hypot(evaluate(f[1],x,y,z=z), evaluate(f[2],x,y,z=z)) *sqrt(2)-1
     elif f[0] == "pow":
-        ans = np.power(evaluate(f[1],x,y), np.floor(10*np.absolute(evaluate(f[2],x,y))))
+        ans = np.power(evaluate(f[1],x,y,z=z), np.floor(10*np.absolute(evaluate(f[2],x,y,z=z))))
     elif f[0] == "copysign":
-        ans = np.copysign(evaluate(f[1],x,y), evaluate(f[2],x,y))
+        ans = np.copysign(evaluate(f[1],x,y,z=z), evaluate(f[2],x,y,z=z))
     else:
         raise TypeError(f[0]+" is not a supported function!")
 
@@ -212,10 +222,12 @@ def generate_movie(filename, x_size=300, y_size=300, t_size=100):
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
     # Functions for red, green, and blue channels - where the magic happens!
-    red_function = build_random_function(8,10,[["x"],["y"],["t"],["t"]])
-    grn_function = build_random_function(8,10,[["x"],["y"],["t"],["t"]])
-    blu_function = build_random_function(8,10,[["x"],["y"],["t"],["t"]])
-
+    red_function = build_random_function(8,10,[["x"],["y"],["t"]])
+    grn_function = build_random_function(8,10,[["x"],["y"],["t"]])
+    blu_function = build_random_function(8,10,[["x"],["y"],["t"]])
+    # Input arrays
+    x = build_x_coordinates(x_size, y_size)
+    y = build_y_coordinates(x_size, y_size)
     # Create image and loop over all pixels
     im = Image.new("RGB", (x_size, y_size))
     pixels = im.load()
@@ -245,24 +257,28 @@ def generate_parametric_art(filename, x_size=1000, y_size=1000):
         filename: string filename for image (should be .png)
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
-    # Functions for parametric, red, green, and blue channels - where the magic happens!
-    para_function = build_random_function(3,5,[["x"],["y"]])
-    red_function = build_random_function(4,6,[["x"],["y"],["t"]])
-    grn_function = build_random_function(4,6,[["x"],["y"],["t"]])
-    blu_function = build_random_function(4,6,[["x"],["y"],["t"]])
-
+    # Functions for red, green, and blue channels - where the magic happens!
+    red_function = build_random_function(4,6,[["x"],["y"],["z"]])
+    grn_function = build_random_function(4,6,[["x"],["y"],["z"]])
+    blu_function = build_random_function(4,6,[["x"],["y"],["z"]])
+    par_function = build_random_function(3,5,[["x"],["y"]])
+    # Input arrays
+    x = build_x_coordinates(x_size, y_size)
+    y = build_y_coordinates(x_size, y_size)
+    # Evaluate the functions
+    par_channel = evaluate(par_function, x, y)
+    red_channel = evaluate(red_function, x, y, z=par_channel)
+    grn_channel = evaluate(grn_function, x, y, z=par_channel)
+    blu_channel = evaluate(blu_function, x, y, z=par_channel)
     # Create image and loop over all pixels
     im = Image.new("RGB", (x_size, y_size))
     pixels = im.load()
     for i in range(x_size):
         for j in range(y_size):
-            x = remap_interval(i, 0, x_size, -1, 1)
-            y = remap_interval(j, 0, y_size, -1, 1)
-            t = evaluate(para_function, x, y, 0)
             pixels[i, j] = (
-                    color_map(evaluate(red_function, x, y, t)),
-                    color_map(evaluate(grn_function, x, y, t)),
-                    color_map(evaluate(blu_function, x, y, t))
+                    color_map(red_channel[i,j]),
+                    color_map(grn_channel[i,j]),
+                    color_map(blu_channel[i,j])
                     )
     im.save(filename+".png")
 
@@ -274,22 +290,26 @@ def generate_art_HSV(filename, x_size=1000, y_size=1000):
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
     # Functions for yrightness, iagenta, and qlue channels - where the magic happens!
-    h_function = build_random_function(7,9,[["x"],["y"]])
-    s_function = build_random_function(7,9,[["x"],["y"]])
-    v_function = build_random_function(7,9,[["x"],["y"]])
-
+    h_function = build_random_function(7,9)
+    s_function = build_random_function(7,9)
+    v_function = build_random_function(7,9)
+    # Input arrays
+    x = build_x_coordinates(x_size, y_size)
+    y = build_y_coordinates(x_size, y_size)
+    # Evaluate the functions
+    h_channel = evaluate(h_function, x, y)
+    s_channel = evaluate(s_function, x, y)
+    v_channel = evaluate(v_function, x, y)
     # Create image and loop over all pixels
     im = Image.new("RGB", (x_size, y_size))
     pixels = im.load()
     for i in range(x_size):
         for j in range(y_size):
-            x = remap_interval(i, 0, x_size, -1, 1)
-            y = remap_interval(j, 0, y_size, -1, 1)
-            rgb_vals = hsv_to_rgb(    # this assumes that y is positive, but i and q can be negative
-                    (evaluate(h_function, x, y)+1)/2,
-                    (evaluate(s_function, x, y)+1)/2,
-                    (evaluate(v_function, x, y)+1)/2
-                    )
+            rgb_vals = hsv_to_rgb(
+                (h_channel[i,j]+1)/2,
+                (s_channel[i,j]+1)/2,
+                (v_channel[i,j]+1)/2
+                )
             pixels[i, j] = (int(255*rgb_vals[0]), int(255*rgb_vals[1]), int(255*rgb_vals[2]))
     im.save(filename+".png")
 
@@ -301,9 +321,9 @@ def generate_art(filename, x_size=1000, y_size=1000):
         x_size, y_size: optional args to set image dimensions (default: 350)
     """
     # Functions for red, green, and blue channels - where the magic happens!
-    red_function = build_random_function(7,9,[["x"],["y"]])
-    grn_function = build_random_function(7,9,[["x"],["y"]])
-    blu_function = build_random_function(7,9,[["x"],["y"]])
+    red_function = build_random_function(7,9)
+    grn_function = build_random_function(7,9)
+    blu_function = build_random_function(7,9)
     # Input arrays
     x = build_x_coordinates(x_size, y_size)
     y = build_y_coordinates(x_size, y_size)
@@ -329,15 +349,12 @@ if __name__ == '__main__':
     doctest.testmod()
 
     # Create some computational art!
-    i = 1
+    i = 0
     while True:
         generate_art(str(i)+"myArt")
-    	#generate_parametric_art(str(i)+"myArtPara")
-    	#if i%5 == 0:
-        #generate_movie("frame")
-    	#generate_art_HSV(str(i)+"myArtHSV")
-    	#if i%20 == 0:
-    	#	generate_movie_from_sound(str(i)+"frame","ImperialMarch.wav")
-    	i = i+1
+    	generate_parametric_art(str(i)+"myArtPara")
+    	generate_art_HSV(str(i)+"myArtHSV")
+    	#generate_movie("frame")
+    	i += 1
 
 print "Done!"
